293e1a1c77a2c5725d6081cb1447132dc3493087
Doron <doronwr@gmail.com>
2017-02-28 11:40:17 +0200

    why?

--- backup.js
+++ backup.js
@@ -1,47 +1,48 @@
 /**
  * Created by Doron Warzager on 26/02/2017.
  */
+// <input type="text" class="form-control" placeholder="Username" aria-describedby="basic-addon1">
+
+// <div class="input-group">
+//   <div class="input-group-btn">
+//   <!-- Button and dropdown menu -->
+// </div>
+// <input type="text" class="form-control" aria-label="...">
+//   </div>
 
 
-// creating the basic first three lists
+// creating ta list
 function createNewList(listsholder,title) {
 
   const nodereferece = document.querySelector('.btn')
   const numberOfTasks = 0;
   const listStringHtml =`
 
-<span class="openerTag ">
+<span class="openerTag " tabindex="0">
 ${title}
 <span class="badge"> ${numberOfTasks} </span> </span>
 <ul class="ulForCards ">
-<li class="card">creat new cards </li>
 </ul>
-<span class="closerTag">Add Card</span>
-`;
+<span class="closerTag" tabindex="0">Add Card</span>`;
 
 
 //creating the new list:
   const basicTemplete = document.createElement('div');
-
   basicTemplete.setAttribute("class", "oneLists" );
   basicTemplete.innerHTML +=listStringHtml;
-
   listsholder.insertBefore(basicTemplete, nodereferece);
-  creatSpanListeners();
 
+  basicTemplete.querySelector('.closerTag').addEventListener('click', createCard);
 
-  //
-  // const test = document.createElement('span');
-  // test.setAttribute("class", "closerTag");
-  //   test.textContent= 'add card';
-  //
-  //
-  //   basicTemplete.appendChild(test);
-
-
+  basicTemplete.querySelector('.openerTag').addEventListener('click', upsdateListName);
 }
 
+function upsdateListName(e) {
+  const nameHolder = e.target;
+  nameHolder.style.backgroundColor = 'white';
 
+  console.log('hello');
+}
 //function to create a card
 function createCard(e){
   const newCard = document.createElement('li');
@@ -51,21 +52,20 @@ function createCard(e){
   newCard.setAttribute("class", "card" );
   newCard.textContent = 'card';
 
-  parentNode.insertBefore(newCard, referenceNode)
+  parentNode.insertBefore(newCard, referenceNode);
+  updateBagde(e, parentNode);
 
 }
 
-
-function creatSpanListeners() {
-  const spans = document.querySelectorAll('.closerTag');
-
-  for (const oneSpan of spans) {
-    oneSpan.addEventListener('click', createCard);
-  }
+function updateBagde(badge, parentNode) {
+  const numberOfCards = badge.target.parentNode.querySelector('.badge');
+  numberOfCards.textContent =parentNode.querySelectorAll('.card').length;
+  // badge.target.parentNode.querySelector('.badge').textContent =  parentNode.querySelectorAll('.card').length;
 }
+
 
 function activeButton() {
-  const button = document.querySelector('.btn')
+  const button = document.querySelector('.btn');
   // createNewList(liholder, 'New List')
   button.addEventListener('click',()=>{
     createNewList(liholder, 'New List');
@@ -80,7 +80,7 @@ createNewList(liholder, 'todo');
 createNewList(liholder, 'QNA');
 
 
-creatSpanListeners();
+// creatSpanListeners();
 activeButton();
 
 
--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 /**
  * Created by Doron Warzager on 26/02/2017.
  */
+// <input type="text" class="form-control" placeholder="Username" aria-describedby="basic-addon1">
+
 
 
 // creating ta list
@@ -10,12 +12,14 @@ function createNewList(listsholder,title) {
 const numberOfTasks = 0;
 const listStringHtml =`
 
-<span class="openerTag ">
-${title}
+
+<span class="openerTag " tabindex="0">
+<span class="tagText" contenteditable="true"><strong>${title}</strong></span>
+
 <span class="badge"> ${numberOfTasks} </span> </span>
 <ul class="ulForCards ">
 </ul>
-<span class="closerTag">Add Card</span>`;
+<span class="closerTag" tabindex="0">Add Card</span>`;
 
 
 //creating the new list:
@@ -24,10 +28,33 @@ basicTemplete.setAttribute("class", "oneLists" );
 basicTemplete.innerHTML +=listStringHtml;
   listsholder.insertBefore(basicTemplete, nodereferece);
 
+
+
   basicTemplete.querySelector('.closerTag').addEventListener('click', createCard);
+
+  //this are the listeners i've added
+  basicTemplete.querySelector('.openerTag').addEventListener('click', upsdateListName);
+
+  basicTemplete.querySelector('.openerTag').addEventListener('blur', upsdateListName);
 }
+//this is the  function i've build to listen
+function upsdateListName(e) {
+  const nameHolder = e.target;
 
-
+  //need to add here a code that changes the span into an inpute
+  //change the span inside the span back into just text - and then just do innerHTML too create there a inpute-  and then finnally turn it back to the span!
+  if(e.type === 'click') {
+    // nameHolder.querySelector('.tagText').textContent= '';
+    nameHolder.style.backgroundColor = 'green';
+    // nameHolder.querySelector('.tagText').style.backgroundColor = 'green'
+  }
+  if(e.type === 'blur') {
+    nameHolder.style.backgroundColor = 'white';
+    nameHolder.querySelector('.tagText').style.backgroundColor = 'white'
+  }
+// add what enter does and so on
+  console.log('hello');
+}
 //function to create a card
 function createCard(e){
   const newCard = document.createElement('li');
@@ -37,14 +64,29 @@ function createCard(e){
   newCard.setAttribute("class", "card" );
   newCard.textContent = 'card';
 
-  parentNode.insertBefore(newCard, referenceNode)
+  parentNode.insertBefore(newCard, referenceNode);
+  updateBagde(e, parentNode);
 
 }
 
+//updating badge number
+function updateBagde(badge, parentNode) {
+
+  const badgenumberOfCards = badge.target.parentNode.querySelector('.badge');
+  const numbeOfCards =parentNode.querySelectorAll('.card').length;
+  badgenumberOfCards.textContent =numbeOfCards;
+
+  //keeping the number in the center of the badge
+  if(numbeOfCards >= 10){
+    badgenumberOfCards.style.paddingLeft = '3px';
+  }
+
+  // badge.target.parentNode.querySelector('.badge').textContent =  parentNode.querySelectorAll('.card').length;
+}
 
 
 function activeButton() {
-  const button = document.querySelector('.btn')
+  const button = document.querySelector('.btn');
   // createNewList(liholder, 'New List')
   button.addEventListener('click',()=>{
     createNewList(liholder, 'New List');
