5d4ac20aeef5c86ce84292b06b97ac812ba19221
Doron <doronwr@gmail.com>
2017-03-01 15:51:24 +0200

    added delete button

--- main.js
+++ main.js
@@ -5,19 +5,30 @@
 
 
 // creating ta list
-function createNewList(listsholder,title) {
+function createNewList(listsholder, title) {
 
-  const nodereferece = document.querySelector('.btn')
+  const nodereferece = document.querySelector('.btn-catcher')
   const numberOfTasks = 0;
 
-  const listStringHtml =`
+  const listStringHtml = `
 
 
 <span class="openerTag " tabindex="0">
-<input class ="inputTag" type="text" style="display: none"></input>
+<!-- bootstrap dropdown menu burtton -->
+<div class="btn-group">
+  <button class="btn btn-default btn-xs dropdown-toggle" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+    <span class="caret"></span>
+  </button>
+  <ul class="dropdown-menu" >
+    <li class="listOption"><a href="#">Delete List</a></li>
+  </ul>
+</div>
+<input class ="inputTag" type="text" style="display: none" maxlength="25"></input>
 <span class="tagText">${title}</span>
 
+
 <span class="badge"> ${numberOfTasks} </span> </span>
+  
 <ul class="ulForCards ">
 </ul>
 <span class="closerTag" tabindex="0">Add Card</span>`;
@@ -25,31 +36,91 @@ function createNewList(listsholder,title) {
 
 //creating the new list:
   const basicTemplete = document.createElement('div');
-  basicTemplete.setAttribute("class", "oneLists" );
-  basicTemplete.innerHTML +=listStringHtml;
+  basicTemplete.setAttribute("class", "oneLists");
+  basicTemplete.innerHTML += listStringHtml;
   listsholder.insertBefore(basicTemplete, nodereferece);
 
+//adding event listeners
+  addEventListeners(basicTemplete);
 
+}
 
+//lists of adding event listeners
+function addEventListeners(basicTemplete) {
+  //listener to create card
   basicTemplete.querySelector('.closerTag').addEventListener('click', createCard);
-
-
-  //this are the listeners i've added
+  //listeners for changing list titles
   basicTemplete.querySelector('.tagText').addEventListener('click', upsdateListName);
   basicTemplete.querySelector('.inputTag').addEventListener('blur', inputLitener);
   basicTemplete.querySelector('.inputTag').addEventListener('keydown', inputLitener);
+
+  basicTemplete.querySelector('.dropdown-toggle').addEventListener('click', listDropDownMenuActions);
+  document.addEventListener('click', dropDownMenuFocusClose);
+  basicTemplete.querySelector('.listOption').addEventListener('click', deleteList);
+
 }
 
+function deleteList(event) {
+  //
+  const listIAmIn = event.target.closest('.oneLists');
+  // const listHolder =  event.target.closest('.mainCardHolders')
+  const nameOfList = event.target.closest('.oneLists').querySelector('.tagText').textContent
+  const deleteCheck = confirm(`to delete ${nameOfList}? are you sure`);
+
+  if (!deleteCheck) {
+    event.target.parentNode.parentNode.style.display = 'none'
+  }
+  else {
+    listIAmIn.remove();
+    // listHolder.removeChild(listIAmIn);
+  }
+}
+
+
+//toggle for drop down menu
+function listDropDownMenuActions(event) {
+  const theMenu = event.currentTarget.parentNode.parentNode.querySelector('.dropdown-menu');
+
+
+  if (theMenu.style.display === 'none' || !theMenu.style.display) {
+    theMenu.style.display = 'block'
+  }
+  else {
+    theMenu.style.display = 'none'
+
+  }
+}
+
+
+//listen to click on all document.
+//ask if element's parent are the button.
+//if yes do nothing. else close menu
+//fix this into catching a not array also//
+function dropDownMenuFocusClose(event) {
+  console.log(event.target.parentNode.querySelectorAll('.dropdown-menu'));
+  const theMenu = event.target.parentNode.querySelectorAll('.dropdown-menu')
+
+  if (event.target.closest('.btn-group')) {
+    console.info('check2');
+//does nothing if button pressed
+  }
+  else {
+
+    for (const oneMenu of theMenu) {
+      oneMenu.style.display = 'none';
+      console.info('check');
+    }
+  }
+}
 
 
 //chang from span to input in list title
 function upsdateListName(e) {
   const nameHolder = e.target;
   const listName = nameHolder.textContent;
-  const nameHolderParent=nameHolder.parentNode;
-  const inputeFiled =nameHolderParent.querySelector('input');
-  // const inputeFiled = document.createElement('input');
-  // inputeFiled.type = 'text'
+  const nameHolderParent = nameHolder.parentNode;
+  const inputeFiled = nameHolderParent.querySelector('input');
+
 
   inputeFiled.value = listName;
   inputeFiled.style.display = "inline-block"
@@ -62,31 +133,35 @@ function upsdateListName(e) {
 
 //listening to the inpute when it appears
 
-function  inputLitener(event){
+function inputLitener(event) {
   let newTitle = event.target.value;
 
-  if(event.type === 'keydown'){
-  if(event.keyCode === 13) {
-    autoReplaceEmptyInputValue(event,newTitle )
+  if (event.type === 'keydown') {
+    if (event.keyCode === 13) {
+      autoReplaceEmptyInputValue(event, newTitle)
+    }
+
   }
-  }
-  if(event.type === 'blur'){
-    autoReplaceEmptyInputValue(event,newTitle )
+  if (event.type === 'blur') {
+    autoReplaceEmptyInputValue(event, newTitle)
   }
 
+
+//if key stronks are nore then X use prevent default
 
 }
 
-function autoReplaceEmptyInputValue(event,newTitle ) {
-  if(newTitle=== '' || newTitle=== ' ') {
+//making sure the title will have a value
+function autoReplaceEmptyInputValue(event, newTitle) {
+  if (newTitle === '' || newTitle === ' ') {
     newTitle = event.target.parentNode.querySelector('.tagText').textContent;
     displayChanger(event, newTitle);
   }
-  else{
+  else {
     displayChanger(event, newTitle);
   }
 }
-
+//shifting from input to span title
 function displayChanger(event, newTitle) {
   const finalTitle = event.target.parentNode.querySelector('.tagText')
 
@@ -98,12 +173,12 @@ function displayChanger(event, newTitle) {
 
 
 //function to create a card
-function createCard(e){
+function createCard(e) {
   const newCard = document.createElement('li');
   const parentNode = e.target.parentNode.childNodes[3]
   const referenceNode = parentNode.querySelector('ul > li:last-child')
 
-  newCard.setAttribute("class", "card" );
+  newCard.setAttribute("class", "card");
   newCard.textContent = 'card';
 
   parentNode.insertBefore(newCard, referenceNode);
@@ -115,25 +190,26 @@ function createCard(e){
 function updateBagde(badge, parentNode) {
 
   const badgenumberOfCards = badge.target.parentNode.querySelector('.badge');
-  const numbeOfCards =parentNode.querySelectorAll('.card').length;
-  badgenumberOfCards.textContent =numbeOfCards;
+  const numbeOfCards = parentNode.querySelectorAll('.card').length;
+  badgenumberOfCards.textContent = numbeOfCards;
 
   //keeping the number in the center of the badge
-  if(numbeOfCards >= 10){
+  if (numbeOfCards >= 10) {
     badgenumberOfCards.style.paddingLeft = '3px';
   }
 }
 
 
 function activeButton() {
-  const button = document.querySelector('.btn');
+  const button = document.querySelector('.btn-catcher');
   // createNewList(liholder, 'New List')
-  button.addEventListener('click',()=>{
+  button.addEventListener('click', () => {
     createNewList(liholder, 'New List');
   })
 }
 
-// creating page
+//        // creating page   //     //      //        / /
+
 const liholder = document.querySelector('#mainCardHolders');
 
 //action that happen when page is loaded
---
+++ polly.js
@@ -0,0 +1,29 @@
+if (window.Element && !Element.prototype.closest) {
+  Element.prototype.closest =
+    function(s) {
+      var matches = (this.document || this.ownerDocument).querySelectorAll(s),
+        i,
+        el = this;
+      do {
+        i = matches.length;
+        while (--i >= 0 && matches.item(i) !== el) {};
+      } while ((i < 0) && (el = el.parentElement));
+      return el;
+    };
+}
+
+if (window.Element && !Element.prototype.closest) {
+  Element.prototype.closest =
+    function(s) {
+      var matches = (this.document || this.ownerDocument).querySelectorAll(s),
+        i,
+        el = this;
+      do {
+        i = matches.length;
+        while (--i >= 0 && matches.item(i) !== el) {};
+      } while ((i < 0) && (el = el.parentElement));
+      return el;
+    };
+}/**
+ * Created by Doron Warzager on 01/03/2017.
+ */
